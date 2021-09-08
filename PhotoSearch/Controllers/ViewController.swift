1e5b93e0e76622e1061da4ca479e386092395d67
Chinmoy Biswas <57076309+ChinmoyBD@users.noreply.github.com>
2021-09-08 20:42:37 +0600

    successful Get data using API

--- PhotoSearch/Controllers/ViewController.swift
+++ PhotoSearch/Controllers/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let urlString = "https://api.unsplash.com/search/photos?page=1&query=office&client_id=PsVtIqTBY0yT4uUjZdRheGCQxNB1YZD66Hqmnwv3DIc"
+    
+    // MARK: - Default Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fetchPhotos()
+    }
+    
+    func fetchPhotos() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            print("Got data!")
+        }
+        
+        task.resume()
     }
 
 
